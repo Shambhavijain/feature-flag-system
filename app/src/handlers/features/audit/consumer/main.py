ee1212248698cf11736264cb2c7894ae291d51b4
Shambhavi Jain <shambhavi0312@gmail.com>
2026-02-10 09:12:09 +0530

    Changes in README.

--- app/src/handlers/features/audit/consumer/main.py
+++ app/src/handlers/features/audit/consumer/main.py
@@ -10,24 +10,32 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     logger.info("AuditConsumer Lambda invoked")
     logger.info(f"Records received: {len(event['Records'])}")
+    
+    batch_item_failures = []
 
     for record in event["Records"]:
-        message = json.loads(record["body"])
-
-        logger.info(
-            f"Processing audit message | "
-            f"feature={message['feature']} | "
-            f"action={message['action']}"
-        )
-
-        table.put_item(
-        Item={
-            "PK": f"AUDIT#{message['feature'].lower()}",
-            "SK": f"LOGS#{message['timestamp']}",
-            "action": message["action"],
-            "actor": message["actor"],
-            "old_value": message.get("old"),
-            "new_value": message.get("new"),
-        }
-    )
+        try:
+            message = json.loads(record["body"])
+            
+            logger.info(
+                f"Processing audit message | "
+                f"feature={message['feature']} | "
+                f"action={message['action']}"
+            )
+            
+            table.put_item(
+                Item={
+                    "PK": f"AUDIT#{message['feature'].lower()}",
+                    "SK": f"LOGS#{message['timestamp']}",
+                    "action": message["action"],
+                    "actor": message["actor"],
+                    "old_value": message.get("old"),
+                    "new_value": message.get("new"),
+                }
+            )
+        except Exception as e:
+            logger.error(f"Failed to process message: {e}")
+            batch_item_failures.append({"itemIdentifier": record["messageId"]})
+    
+    return {"batchItemFailures": batch_item_failures}
 
